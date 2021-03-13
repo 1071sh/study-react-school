f211ac8761b8e4acd8a0c8ee45eb09bc355b4301
nishida <i9i9ksy@gmail.com>
2021-03-13 23:49:52 +0900

    検索フォーム途中

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import Booklist from "./components/Booklist";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import axios from "axios";
+import { Button } from "@material-ui/core";
 
 // 入力値に`books`を追加して出力するシンプルな関数を定義
 const getDataFormAPI = async (keyword) => {
@@ -12,13 +13,17 @@ const getDataFormAPI = async (keyword) => {
 
 function App() {
     const languages = ["React", "Vue", "Angular"]; // 追加
+    const [text, setText] = useState("");
+    const handleChange = (e) => {
+        setText(() => e.target.value);
+    };
     return (
         <BrowserRouter>
             <div>
                 <h1>React App</h1>
                 <ul>
                     <li>
-                        <Link to="/">React</Link>
+                        <Link to="/react">React</Link>
                     </li>
                     <li>
                         <Link to="/vue">Vue</Link>
@@ -26,10 +31,31 @@ function App() {
                     <li>
                         <Link to="/angular">Angular</Link>
                     </li>
+                    <li>
+                        <input
+                            value={text}
+                            onChange={handleChange}
+                            type="text"
+                            placeholder="本のタイトル"
+                        />
+                        <Link to={text} onClick={() => handleChange}>
+                            <Button variant="contained" color="primary">
+                                書籍を探す
+                            </Button>
+                        </Link>
+                    </li>
                 </ul>
                 <Route
-                    exact
-                    path="/"
+                    path="/:text"
+                    render={(props) => (
+                        <Booklist
+                            language={text}
+                            getData={(keyword) => getDataFormAPI(keyword)} // getDataという名前で関数を渡す
+                        />
+                    )}
+                />
+                <Route
+                    path="/react"
                     render={(props) => (
                         <Booklist
                             language={languages[0]}
--- src/components/Booklist.jsx
+++ src/components/Booklist.jsx
@@ -20,20 +20,26 @@ const BookList = (props) => {
                                     {x.volumeInfo.title}
                                 </a>
                             </h3>
-                            <p>
+                            {/* <p>
                                 {x.volumeInfo.authors.length >= 2
                                     ? x.volumeInfo.authors[0] + " 他"
                                     : x.volumeInfo.authors[0]}
                                 ／著
+                            </p> */}
+                            <p>
+                                {x.saleInfo.saleability === "FOR_SALE"
+                                    ? x.saleInfo.listPrice.amount.toLocaleString() +
+                                      "円"
+                                    : "非売品"}
                             </p>
                             <a target="_blannk" href={x.volumeInfo.infoLink}>
                                 <img
                                     src={
-                                        x.volumeInfo.imageLinks.thumbnail
+                                        x.volumeInfo.imageLinks
                                             ? x.volumeInfo.imageLinks.thumbnail
-                                            : ""
+                                            : "http://placehold.jp/150x150.png?text=sample"
                                     }
-                                    alt="x.volumeInfo.title"
+                                    alt={x.volumeInfo.title}
                                 />
                             </a>
                         </li>
