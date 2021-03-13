6b7efa07f4a07efbbe851ec4d16e41607c7d4208
nishida <i9i9ksy@gmail.com>
2021-03-13 20:28:42 +0900

    レイアウト途中

--- src/components/Booklist.jsx
+++ src/components/Booklist.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 const BookList = (props) => {
     const [bookData, setBookData] = useState(null);
     useEffect(() => {
-        const result = props
+        props
             .getData?.(props.language)
             .then((response) => setBookData(response));
     }, [props]);
@@ -14,7 +14,29 @@ const BookList = (props) => {
                     <p>now loading...</p>
                 ) : (
                     bookData.data.items.map((x, index) => (
-                        <li key={index}>{x.volumeInfo.title}</li>
+                        <li key={index}>
+                            <h3>
+                                <a href={x.volumeInfo.infoLink}>
+                                    {x.volumeInfo.title}
+                                </a>
+                            </h3>
+                            <p>
+                                {x.volumeInfo.authors.length >= 2
+                                    ? x.volumeInfo.authors[0] + " 他"
+                                    : x.volumeInfo.authors[0]}
+                                ／著
+                            </p>
+                            <a target="_blannk" href={x.volumeInfo.infoLink}>
+                                <img
+                                    src={
+                                        x.volumeInfo.imageLinks.thumbnail
+                                            ? x.volumeInfo.imageLinks.thumbnail
+                                            : ""
+                                    }
+                                    alt="x.volumeInfo.title"
+                                />
+                            </a>
+                        </li>
                     ))
                 )}
             </ul>
